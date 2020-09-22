555279405f29068a1bf13860961182640ff83d0d
Matthew Sallin <msallin@gmail.com>
2020-09-22 04:19:54 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,9 @@
 def square_array(array)
   newArr = []
-  while array.length do |index|
+  counter = 0
+  while array.length do
     newArr[index] = array[index]**
+    counter +=1
   end
   newArr
 end
